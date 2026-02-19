71ca03b18c1087b2301883e9152bb8cf9f6f7911
anasse84 <anassejob@gmail.com>
2026-02-19 03:53:39 +0100

    update

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
  * Version: 1.1.0
  */
 
-const CACHE_VERSION = 'bentopdf-v31';
+const CACHE_VERSION = 'bentopdf-v33-debug';
 const CACHE_NAME = `${CACHE_VERSION}-static`;
 
 const getBasePath = () => {
